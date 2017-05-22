124822e690d1bce5af9ff6969098a2897c2917b9
osboxes.org <r.streefkerk@gmail.com>
2017-05-22 16:43:11 +0700

    stack and python update, pass variable

--- generate_static_site.py
+++ generate_static_site.py
@@ -2,28 +2,28 @@
 """ Generates static site from CodeCommit source and deploys to production S3 Bucket """
 
 from __future__ import print_function
+import os
 import zipfile
 import tempfile
 import shutil
 import subprocess
 import traceback
 import boto3
+from botocore.client import Config
 
 CP = boto3.client('codepipeline')
-S3 = boto3.client('s3')
+S3 = boto3.client('s3', config=Config(signature_version='s3v4'))
 
 def setup(event):
     """ Setup all properties"""
     job_id = event['CodePipeline.job']['id']
     job_data = event['CodePipeline.job']['data']
     input_artifact = job_data['inputArtifacts'][0]
-    output_artifact = job_data['outputArtifacts'][0]
     from_bucket = input_artifact['location']['s3Location']['bucketName']
     from_key = input_artifact['location']['s3Location']['objectKey']
-    to_bucket = output_artifact['location']['s3Location']['bucketName']
-    to_key = output_artifact['location']['s3Location']['objectKey']
+    to_bucket = os.environ['SiteBucket']
 
-    return (job_id, from_bucket, from_key, to_bucket, to_key)
+    return (job_id, from_bucket, from_key, to_bucket)
 
 def download_source(from_bucket, from_key, source_dir):
     """ Download source code to be generated """
@@ -32,11 +32,12 @@ def download_source(from_bucket, from_key, source_dir):
         with zipfile.ZipFile(tmp_file.name, 'r') as zipobj:
             zipobj.extractall(source_dir)
 
-def upload_site(site_dir, to_bucket, to_key):
-    """ Upload generated code to S3 site bucket """
-    with tempfile.NamedTemporaryFile() as tmp_file:
-        site_zip_file = shutil.make_archive(tmp_file.name, 'zip', site_dir)
-        S3.upload_file(site_zip_file, to_bucket, to_key)
+def upload_site(site_dir, to_bucket):
+    """ Upload generated code to S3 site bucket, clean existing data on copy """
+    command = ["./aws", "s3", "sync", "--acl", "public-read", "--delete",
+               site_dir + "/", "s3://" + to_bucket + "/"]
+    print(command)
+    print(subprocess.check_output(command, stderr=subprocess.STDOUT))
 
 def generate_static_site(source_dir, site_dir):
     """Generate static site using hugo."""
@@ -53,13 +54,13 @@ def generate_static_site(source_dir, site_dir):
 def handler(event, context):
     """ Program event flow"""
     try:
-        (job_id, from_bucket, from_key, to_bucket, to_key) = setup(event)
+        (job_id, from_bucket, from_key, to_bucket) = setup(event)
         source_dir = tempfile.mkdtemp()
         site_dir = tempfile.mkdtemp()
 
         download_source(from_bucket, from_key, source_dir)
         generate_static_site(source_dir, site_dir)
-        upload_site(site_dir, to_bucket, to_key)
+        upload_site(site_dir, to_bucket)
         CP.put_job_success_result(jobId=job_id)
 
     except Exception as exception:
